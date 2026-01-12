e01847a4e7845ce0107c97a93d7f207c6857787e
Tim Griesbach <tim.griesbach@uni-bonn.de>
2026-01-12 19:44:32 +0100

    Do not use hard-coded lib folder

--- cmake/pkgconf.cmake
+++ cmake/pkgconf.cmake
@@ -8,10 +8,10 @@ set(pc_filename p4est-${git_version}.pc)
 configure_file(${CMAKE_CURRENT_LIST_DIR}/pkgconf.pc.in ${pc_filename} @ONLY)
 
 install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${pc_filename}
-  DESTINATION lib/pkgconfig)
+  DESTINATION ${CMAKE_INSTALL_LIBDIR}/pkgconfig)
 
 set(pc_target ${pc_filename})
-set(pc_link ${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/p4est.pc)
+set(pc_link ${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}/pkgconfig/p4est.pc)
 
 install(CODE "execute_process( \
     COMMAND ${CMAKE_COMMAND} -E create_symlink \
